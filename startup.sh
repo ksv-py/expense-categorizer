ec5c38e363746af525251c6089ff6c4c3a208bbf
ksv-py <keshavjangid301@gmail.com>
2025-07-12 00:58:10 +0530

    startup.sh created

--- startup.sh
+++ startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+set -e
+echo "creating venv###############################################################"
 python -m venv antenv
+echo "activating venv #################################################################"
 source antenv/bin/activate
+echo "Installing requirements#################################################################"
 pip install -r requirements.txt
+echo "executing Gunicorn #########################################################################"
 exec gunicorn app:app
